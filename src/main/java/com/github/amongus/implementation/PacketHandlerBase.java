fd75fa36c5b6c891da6c15aa2ca4d5975d3a300f
Brandon Li <40838203+PulseBeat02@users.noreply.github.com>
2020-11-22 22:31:41 -0500

    Added Some Shit

---
+++ src/main/java/com/github/amongus/implementation/PacketHandlerBase.java
@@ -0,0 +1,9 @@
+package com.github.amongus.implementation;
+
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+
+public interface PacketHandlerBase {
+    public void sendSabatogePacket(Player p, int warningBlocks);
+    public void sendGlowPacket(Player p, Entity glowing);
+}
